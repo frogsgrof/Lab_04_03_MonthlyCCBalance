f27098380f52b03719e6cf3712c116fae0245d64
Isabelle Henson <hensonir@mail.uc.edu>
2023-02-11 00:04:19 -0500

    Lab 04 03

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        // Directions:
-        /*
-        A program that starts with a credit card balance of $5000 and
-        then calculates the interest at a rate of 17%.  Assume the user
-        does not make any payments.  Display the interest due after
-        one month and again after two months.
-         */
 
         final double INTEREST_RATE = 0.17; // stores rate of interest
 
